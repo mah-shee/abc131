3f11c4ad077c74a9afda9db61afcf7986b30aa44
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-21 00:00:32 +0900

    Done d

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -4,5 +4,17 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        mut time: [(usize, usize); n]
     }
+    time.sort_by(|a, b| a.1.cmp(&b.1));
+    let mut a = 0;
+    for i in 0..n {
+        a += time[i].0;
+        if a > time[i].1 {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
